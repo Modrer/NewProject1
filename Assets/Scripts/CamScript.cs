d062769701f8ce6765ae7db21b4d9cdf636e96f7
Modrer <48948138+Modrer@users.noreply.github.com>
2019-05-08 21:55:53 +0300

    Initial commit

---
+++ Assets/Scripts/CamScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour
+{
+    public GameObject Camera;
+   
+    
+    float camSpeed = 50;
+    float gold = 1000;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, 100, 100), "Gold: " + gold);
+        if(GUI.Button(new Rect(400,400,100,50), "Завершити хід"))
+        {
+            //endOfHod
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        float scrool = Input.GetAxis("Mouse ScrollWheel");
+
+
+        gold += moveX + moveY;
+        Camera.transform.position += new Vector3(moveX * camSpeed, moveY * camSpeed , scrool*camSpeed);
+        
+       
+
+        
+    }
+
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject menoObject;
+    
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartClick()
+    {
+        Application.LoadLevel(1);
+
+    }
+
+    public void OnExitClick()
+    {
+        
+        Application.Quit ();
+
+    }
+
+}
